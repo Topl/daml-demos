7ea8e89488ca8682a682de395d020ae840b5a0bf
tuxman <n.edmonds@topl.me>
2021-10-13 12:50:56 -0500

    Initial create-daml-app commit

---
+++ ui/src/components/Assets.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Form } from 'semantic-ui-react'
+import { Party } from '@daml/types'
+import { User } from '@daml.js/create-daml-app'
+import { useParty, useLedger } from '@daml/react'
+import { List, ListItem } from 'semantic-ui-react'
+import { useStreamQuery } from '@daml/react'
+
+const Assets: React.FC = () => {
+    const sender = useParty()
+    const ledger = useLedger()
+    const messagesResult = useStreamQuery(User.Message)
+
+    return (
+        <List relaxed>
+            {messagesResult.contracts.map(message => {
+        const {sender, receiver, content} = message.payload;
+        return (
+          <ListItem
+            className='test-select-message-item'
+            key={message.contractId}>
+            <strong>{sender} &rarr; {receiver}:</strong> {content}
+          </ListItem>
+        );
+            })}
+        </List>
+    )
+}
+
+export default Assets
---
+++ ui/src/components/NewAsset.tsx
@@ -0,0 +1,59 @@
+
+import React from 'react'
+import { Form, Button } from 'semantic-ui-react'
+import { Party } from '@daml/types'
+import { User, AssetCreation } from '@daml.js/create-daml-app'
+import { useParty, useLedger } from '@daml/react'
+import { List, ListItem } from 'semantic-ui-react'
+import { useStreamQuery } from '@daml/react'
+
+
+const NewAsset: React.FC = () => {
+    const sender = useParty()
+    const [id, setName] = React.useState("");
+    const [quantity, setAmount] = React.useState("0");
+    const [isSubmitting, setIsSubmitting] = React.useState(false);
+    const ledger = useLedger()
+
+    const createAsset = async (event: React.FormEvent) => {
+      try {
+        event.preventDefault()
+        const receiver = sender
+        setIsSubmitting(true)
+        await ledger.exerciseByKey(AssetCreation.User.CreateAsset, receiver, {receiver, id, quantity})
+        setName("")
+        setAmount("")
+      } catch (error) {
+        alert(`Error creating asset:\n${JSON.stringify(error)}`)
+      } finally {
+        setIsSubmitting(false)
+      }
+    }
+
+    return (
+      <Form onSubmit={createAsset}>
+        <Form.Input
+          className='test-select-message-content'
+          placeholder="Asset name"
+          value={id}
+          onChange={event => setName(event.currentTarget.value)}
+        />
+        <Form.Input
+          className='test-select-message-content'
+          placeholder="Asset quantity"
+          value={quantity}
+          onChange={event => setAmount(event.currentTarget.value)}
+        />
+        <Button
+          fluid
+          className='test-select-message-send-button'
+          type="submit"
+          disabled={isSubmitting || id === "" || quantity === "0"}
+          loading={isSubmitting}
+          content="Send"  
+        />
+      </Form>
+    )
+}
+
+export default NewAsset
