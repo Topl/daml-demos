3729bae861c9b1b4566be5a8405509d77b9104a6
Edmundo Lopez Bobeda <edmundo@lopezbobeda.net>
2022-12-02 16:48:32 -0400

    TSDK-191 Add balance processor to broker

--- scala-daml-broker-app/src/main/scala/broker/BalanceProcessorModule.scala
+++ scala-daml-broker-app/src/main/scala/broker/BalanceProcessorModule.scala
@@ -8,17 +8,19 @@ import cats.effect.IO
 import co.topl.daml.assets.processors.AssetTransferRequestProcessor
 import co.topl.daml.assets.processors.SignedAssetTransferRequestProcessor
 import co.topl.daml.assets.processors.AssetTransferRequestEd25519Processor
+import co.topl.daml.api.model.topl.asset.AssetBalanceRequest
+import co.topl.daml.assets.processors.AssetBalanceRequestProcessor
 
-trait AssetTransferProcessorRegistrationModule {
+trait BalanceProcessorModule {
 
-  def registerAssetTransferRequestProcessor()(implicit
+  def registerAssetBalanceProcessor()(implicit
       transactions: Flowable[Transaction],
       damlAppContext: DamlAppContext,
       toplContext: ToplContext
   ) =
     for {
       transferProcessor <- IO(
-        new AssetTransferRequestEd25519Processor(
+        new AssetBalanceRequestProcessor(
           damlAppContext,
           toplContext,
           3000,
@@ -29,28 +31,4 @@ trait AssetTransferProcessorRegistrationModule {
       _ <- IO(transactions.forEach(transferProcessor.processTransaction))
     } yield ()
 
-  def registerSignedAssetTransferRequestProcessor()(implicit
-      transactions: Flowable[Transaction],
-      damlAppContext: DamlAppContext,
-      toplContext: ToplContext
-  ) = {
-    var i = 0
-    for {
-      transferProcessor <- IO(
-        new SignedAssetTransferRequestProcessor(
-          damlAppContext,
-          toplContext,
-          3000,
-          1,
-          () => {
-            i = i + 1
-            i.toString()
-          },
-          (x, y) => true,
-          t => true
-        )
-      )
-      _ <- IO(transactions.forEach(transferProcessor.processTransaction))
-    } yield ()
-  }
 }
