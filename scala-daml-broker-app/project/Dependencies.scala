fd0b5efe3cf0e00cb8ab81bee603df6f3cba2524
Edmundo Lopez <e.lopez@topl.me>
2023-01-05 15:28:43 -0400

    TSDK-206 Fix sonar error

--- scala-daml-broker-app/project/Dependencies.scala
+++ scala-daml-broker-app/project/Dependencies.scala
@@ -1,11 +1,14 @@
 import sbt._
 
 object Dependencies {
+
+  lazy val toplOrg = "co.topl"
+
   lazy val bramblVersion = "1.10.2"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
-  lazy val brambl = "co.topl" %% "brambl" % bramblVersion
-  lazy val bramblCommon = "co.topl" %% "common" % bramblVersion
+  lazy val brambl = toplOrg %% "brambl" % bramblVersion
+  lazy val bramblCommon = toplOrg %% "common" % bramblVersion
   lazy val catEffects = "org.typelevel" %% "cats-effect" % "3.3.12"
-  lazy val toplDaml = "co.topl" % "daml-bifrost-module" % "0.1.0"
+  lazy val toplDaml = toplOrg % "daml-bifrost-module" % "0.1.0"
   lazy val slf4j = "org.slf4j" % "slf4j-simple" % "2.0.5"
 }
